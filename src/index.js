70259b2ced6a21b32f49868a0d33325c44558646
DangerKrip <kostyashka2003@gmail.com>
2025-10-13 22:11:23 +0300

    Project set up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-const message = "Hello World";
+import express from 'express';
 
-console.log(message);
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleDateString()}`);
+  next();
+});
+
+app.use('{/*any}', (req, res, next) => {
+  res.status(404).json({ message: 'Path not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Get method performed' });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port: ', PORT);
+});
